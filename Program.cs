4f8cbdd39fb6b197aa89bb31a5c52754278504e0
LeAnhQuan91 <quanthao03019791@gmail.com>
2024-04-09 20:33:07 +0700

    GTLN
    GTLN

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+         Console.WriteLine("Nhap 1 so vao man hinh");
+          int size = 0;
+         
+          do
+          {
+            size = Convert.ToInt32(Console.ReadLine());
+            
+             
+          }  
+          while (size > 20);        
+          int[] myArray = new int[size];
+          for (int i = 0;i< myArray.Length; i++)
+          {
+            myArray[i] = Convert.ToInt32(Console.ReadLine());
+          }
+for (int i = 0;i < myArray.Length; i++)
+          {
+            Console.WriteLine(myArray[i] +",");
+          }
+            int maxValue = 0;
+            for (int i = 0;i < myArray.Length; i++)
+            {
+          
+          if(maxValue<myArray[i])
+          {
+            maxValue = myArray[i];
+          }
+          Console.WriteLine("gia tri lon nhat cua mang la:"+maxValue);
+    }
+    }
+}
